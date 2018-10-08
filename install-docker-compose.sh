a5465ce42116c814f853388a7f57bf049ee52285
Abdullah Eroglu <abdullah@maviucak.com>
2018-10-08 13:22:03 +0300

    initial commit

---
+++ install-docker-compose.sh
@@ -0,0 +1,6 @@
+sudo yum update
+sudo yum install -y docker
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
---
+++ jenkins.sh
@@ -0,0 +1,4 @@
+docker build -t myjenkins .
+docker volume create jenkins-log
+docker volume create jenkins-data
+docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master --mount source=jenkins-log,target=/var/log/jenkins --mount source=jenkins-data,target=/var/jenkins_home -d myjenkins
