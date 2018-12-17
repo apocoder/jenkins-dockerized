968aca525deb620907adf94d0a2cb5d1fe9a4cdf
Abdullah Eroglu <apocoder@gmail.com>
2018-12-17 17:23:26 +0300

    Update image-cleaner.sh

--- image-cleaner.sh
+++ image-cleaner.sh
@@ -1,9 +1,4 @@
 set +e
 df -h
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'yny-places-dev') --force
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'yny-places-prod') --force
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'yny-backoffice-dev') --force
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'yny-backoffice-prod') --force
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'yny-image-filter-dev') --force
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'yny-image-filter-prod') --force
+docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' |grep 'image-name') --force
 df -h
